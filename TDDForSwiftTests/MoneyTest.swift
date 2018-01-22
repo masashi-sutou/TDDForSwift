181aeadf6aba7e0c09fbfeef28fff2050461a005
Masashi Sutou <sutou.masasi@gmail.com>
2018-01-23 00:34:06 +0900

    第1章 MoneyTestを作成

---
+++ TDDForSwiftTests/MoneyTest.swift
@@ -0,0 +1,27 @@
+//
+//  MoneyTest.swift
+//  TDDForSwiftTests
+//
+//  Created by 須藤将史 on 2018/01/23.
+//  Copyright © 2018年 須藤将史. All rights reserved.
+//
+
+import XCTest
+@testable import TDDForSwift
+
+class MoneyTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testMulitiplication() {
+        let five: Dollar = Dollar(5)
+        five.times(2)
+        XCTAssertEqual(10, five.amount)
+    }
+}
