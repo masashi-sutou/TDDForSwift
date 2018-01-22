370559277cc0f7b1c673291e184a02a882f8b9d5
Masashi Sutou <sutou.masasi@gmail.com>
2018-01-23 02:38:06 +0900

    第5章 DollarクラスをコピーしてFrancクラスを作成

---
+++ TDDForSwift/Source/Franc.swift
@@ -0,0 +1,31 @@
+//
+//  Franc.swift
+//  TDDForSwift
+//
+//  Created by 須藤将史 on 2018/01/23.
+//  Copyright © 2018年 須藤将史. All rights reserved.
+//
+
+import Foundation
+
+class Franc: Equatable {
+    
+    private var amount: Int
+    
+    init(_ amount: Int) {
+        self.amount = amount
+    }
+    
+    func times(_ multiplier: Int) -> Franc {
+        return Franc(amount * multiplier)
+    }
+    
+    func equals<T: Equatable>(_ object: T) -> Bool {
+        let franc: Franc = object as! Franc
+        return amount == franc.amount
+    }
+    
+    static func ==(lhs: Franc, rhs: Franc) -> Bool {
+        return lhs.amount == rhs.amount
+    }
+}
