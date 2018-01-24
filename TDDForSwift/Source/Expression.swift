52dfc7ba9929f9c9e4a6d4ddd9f19df8105d8d57
Masashi Sutou <sutou.masasi@gmail.com>
2018-01-24 22:46:34 +0900

    第12章 Expressionを作成する - SwiftはprotocolでJavaのinterfaceを表現する

---
+++ TDDForSwift/Source/Expression.swift
@@ -0,0 +1,12 @@
+//
+//  Expression.swift
+//  TDDForSwift
+//
+//  Created by 須藤将史 on 2018/01/24.
+//  Copyright © 2018年 須藤将史. All rights reserved.
+//
+
+import Foundation
+
+protocol Expression {
+}
